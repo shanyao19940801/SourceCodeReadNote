1b18018477fd554df6716ebb434980521612669e
shanyaoself <sqshanyao@163.com>
2018-10-26 16:55:52 +0800

    no message

---
+++ sourcecodestudy/java_code/src/jdk/util/concurrent/test/LongTest.java
@@ -0,0 +1,13 @@
+package jdk.util.concurrent.test;
+
+public class LongTest {
+    public void test(long l, int m) {
+
+    }
+    public static void main(String[] args) {
+        LongTest longTest = new LongTest();
+        int a = 1;
+        long b = 3L;
+        longTest.test(b,a);
+    }
+}
